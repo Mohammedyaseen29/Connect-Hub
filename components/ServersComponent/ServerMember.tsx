6716ef905a9b4d19468d814ec40f10956af4dcca
Yaseen <yaseen17861786@gmail.com>
2024-04-20 20:30:36 +0530

    ^1.0.0

--- components/ServersComponent/ServerMember.tsx
+++ components/ServersComponent/ServerMember.tsx
@@ -18,16 +18,16 @@ const roleIconMap = {
 }
 
 const ServerMember = ({member,server}:serverMemberProps) => {
-    const icon = roleIconMap[member.role]
+    const icon = roleIconMap[member.role as keyof typeof roleIconMap]
     const params = useParams();
     const router = useRouter();
     const onclick = ()=>{
-        router.push(`/servers/${params.serverId}/conversations/${member.id}`)
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
     }
     return (
-        <button onClick={()=>onclick()} className={cn("flex items-center rounded-md mb-1 w-full group hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 p-2 transition gap-x-1.5",params.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
+        <button onClick={()=>onclick()} className={cn("flex items-center rounded-md mb-1 w-full group hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 p-2 transition gap-x-1.5",params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
             <UserAvatar src={member.profile.imageUrl} className="w-8 h-8 md:h-8 md:w-8"/>
-            <p className={cn("font-semibold text-sm text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 hover:dark:text-zinc-300 transition",params.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white")}>{member.profile.name}</p>
+            <p className={cn("font-semibold text-sm text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 hover:dark:text-zinc-300 transition",params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white")}>{member.profile.name}</p>
             {icon}
         </button>
     )
